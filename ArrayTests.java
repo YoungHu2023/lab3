35367459a757cc81349c34372b795ef96d84e0af
Yang Hu <huyang2022@126.com>
2024-01-29 23:23:35 -0800

    lab activity

--- ArrayTests.java
+++ ArrayTests.java
@@ -9,10 +9,30 @@ public class ArrayTests {
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
 
+  @Test 
+	public void testReverseInPlace2() {
+    int[] input = {1,2,3};
+    int[] expected = {3,2,1};
+    ArrayExamples.reverseInPlace(input);
+    assertArrayEquals(expected, input);
+	}
 
   @Test
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testReversed2() {
+    int[] input = {10,9,8,7,6,5,4,3,2,1};
+    assertArrayEquals(new int[]{1,2,3,4,5,6,7,8,9,10}, ArrayExamples.reversed(input));
+  }
+  @Test
+  public void testReversed3() {
+    int[] input = {3,2,1};
+    ArrayExamples.reversed(input);
+    assertArrayEquals(new int[]{3,2,1}, input);
+  }
 }
+
